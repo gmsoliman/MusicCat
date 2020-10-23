2ed8bb2a7b17dc5c343119899df870c66553f465
BHollis1 <betsy.hollis1@gmail.com>
2020-10-23 10:03:19 -0400

    added Album Models/added DbSet<Album>

--- MusicCat.Data/IdentityModels.cs
+++ MusicCat.Data/IdentityModels.cs
@@ -1,8 +1,10 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using MusicCat.Data;
 
 namespace MusicCat.WebAPI.Models
 {
@@ -29,5 +31,7 @@ namespace MusicCat.WebAPI.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Album> Albums { get; set; }
     }
 }
