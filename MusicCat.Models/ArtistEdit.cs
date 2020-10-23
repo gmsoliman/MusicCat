a29e53932a1820f5515867df86a735468a19ab71
Tom <tomrambo95@yahoo.com>
2020-10-23 10:03:13 -0400

    Added artistCreate/ListItem/Details/Edit Models to MusicCat.Models

---
+++ MusicCat.Models/ArtistEdit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicCat.Models
+{
+    public class ArtistEdit
+    {
+        //tr
+        public string ArtistName { get; set; }
+        public string Hometown { get; set; }
+        //tr
+    }
+}
