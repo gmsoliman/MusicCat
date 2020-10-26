584d32241cb696d160e6bab1e9831f60b1b7dc34
gmsoliman <gmsoliman13@gmail.com>
2020-10-26 11:35:27 -0400

    added GenreController / fixed name of delete method for GenreService

---
+++ MusicCat.WebAPI/Controllers/GenreController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNet.Identity;
+using MusicCat.Models;
+using MusicCat.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MusicCat.WebAPI.Controllers
+{
+    public class GenreController : ApiController
+    {
+        private GenreService CreateGenreService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var genreService = new GenreService(userId);
+            return genreService;
+        }
+        public IHttpActionResult Get()
+        {
+            GenreService genreService = CreateGenreService();
+            var genres = genreService.GetAllGenres();
+            return Ok(genres);
+        }
+        public IHttpActionResult Get(int id)
+        {
+            GenreService genreService = CreateGenreService();
+            var genre = genreService.GetGenreById(id);
+            return Ok(genre);
+        }
+        public IHttpActionResult Post(GenreCreate genre)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateGenreService();
+
+            if (!service.CreateGenre(genre))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Put(GenreEdit genre)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateGenreService();
+
+            if (!service.UpdateGenre(genre))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateGenreService();
+
+            if (!service.DeleteGenre(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
