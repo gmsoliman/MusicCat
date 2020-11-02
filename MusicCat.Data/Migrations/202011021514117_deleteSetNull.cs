9f637cf357c3dbe3324ef6b0d34408cc3660baa2
Tom <tomrambo95@yahoo.com>
2020-11-02 11:45:05 -0500

    Added model builders to IdModels.cs/added to song put method

---
+++ MusicCat.Data/Migrations/202011021514117_deleteSetNull.cs
@@ -0,0 +1,16 @@
+namespace MusicCat.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class deleteSetNull : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
